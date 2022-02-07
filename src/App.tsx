7af128bf8c8605662b861ed1359a9a11981cad3f
Tor Martin Wang <tmwang@stud.ntnu.no>
2022-02-07 15:51:06 +0100

    Fjernet https redirect; kræsjet alt

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import UseRedirectToHttps from "./hooks/useRedirectToHttps";
 
 import LoginPage from "./components/pages/login/LoginPage";
 
 function App() {
-  UseRedirectToHttps();
+  // UseRedirectToHttps();
   return (
     <div className="App" style={{ width: "100vw", height: "100vh" }}>
       <Routes>
