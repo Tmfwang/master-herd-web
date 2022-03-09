75c0f8f199ea51adab8c530d8e70ca617893b886
Tor Martin Wang <tmwang@stud.ntnu.no>
2022-03-09 20:14:07 +0100

    Småfiksing på loginsiden

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -3,16 +3,15 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+// @ts-ignore
+import icon from "../../../assets/iconLandscape.png";
 
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -126,9 +125,11 @@ const LoginPage: React.FC<LoginPageProps> = ({}) => {
                 alignItems: "center",
               }}
             >
-              <Avatar sx={{ m: 1, bgcolor: "green" }}>
-                <LockOutlinedIcon />
-              </Avatar>
+              <img
+                width={200}
+                src={icon}
+                style={{ marginRight: "-15px", marginBottom: "40px" }}
+              ></img>
               <Typography component="h1" variant="h5">
                 Logg inn
               </Typography>
@@ -177,12 +178,8 @@ const LoginPage: React.FC<LoginPageProps> = ({}) => {
                   Logg inn
                 </Button>
                 <Grid container>
-                  <Grid item xs>
-                    <Link href="#" variant="body2">
-                      Glemt passord?
-                    </Link>
-                  </Grid>
-                  <Grid item xs>
+                  <Grid item xs></Grid>
+                  <Grid item>
                     <Link
                       style={{ cursor: "pointer" }}
                       onClick={() => setIsRegistering(true)}
@@ -201,7 +198,7 @@ const LoginPage: React.FC<LoginPageProps> = ({}) => {
             animationOut="slideOutRight"
             isVisible={isRegistering}
             animateOnMount={false}
-            style={{ marginTop: "-500px" }}
+            style={{ marginTop: "-550px" }}
           >
             <Box
               sx={{
@@ -212,9 +209,12 @@ const LoginPage: React.FC<LoginPageProps> = ({}) => {
                 alignItems: "center",
               }}
             >
-              <Avatar sx={{ m: 1, bgcolor: "blue" }}>
-                <AccountCircleOutlinedIcon />
-              </Avatar>
+              <img
+                width={200}
+                src={icon}
+                style={{ marginRight: "-15px", marginBottom: "40px" }}
+              ></img>
+
               <Typography component="h1" variant="h5">
                 Registrer ny bruker
               </Typography>
@@ -297,10 +297,8 @@ const LoginPage: React.FC<LoginPageProps> = ({}) => {
                   Registrer bruker
                 </Button>
                 <Grid container>
-                  <Grid item xs>
-                    <Link href="#" variant="body2"></Link>
-                  </Grid>
-                  <Grid item xs>
+                  <Grid item xs></Grid>
+                  <Grid item>
                     <Link
                       style={{ cursor: "pointer" }}
                       onClick={() => setIsRegistering(false)}
